24cb9d6e332cb4259c6d7a601db0a782462acc67
xuanhathu <dangchithinhcungson@gmail.com>
2024-03-16 22:42:23 +0700

    Xong profile & 80% nav function

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,67 +1,29 @@
-window.addEventListener('hashchange', function() {
-    var hash = window.location.hash;
-    var homePage = document.querySelector('.home-page');
-    var blog_page = this.document.querySelector('.blog-page');
-    if (hash === '#blog') {
-        homePage.classList.add('none');
-        blog_page.classList.remove('none');
-    } else {
-        homePage.classList.remove('none');
+var blog_page = this.document.querySelector('.blog-page');
+create_blog_icon = document.querySelector('.nav_edit');
+create_blog_icon.addEventListener('click', function() {
+    if (document.querySelector('.home-page').classList.contains('none')) {
+        document.querySelector('.home-page').classList.remove('none');
         blog_page.classList.add('none');
-
-    }
-    console.log(hash)
-    // Kiểm tra nếu không có hash hoặc hash là một hash rỗng thì hiển thị trang chính
-    if (!hash || hash === '#') {
-        homePage.classList.remove('none');
-        this.document.querySelector('.blog-page').classList.add('none');
-    }
-});
-
-// Đảm bảo rằng trạng thái ban đầu của trang được xử lý đúng
-window.addEventListener('DOMContentLoaded', function() {
-    var hash = window.location.hash;
-    var homePage = document.querySelector('.home-page');
-
-    if (!hash || hash === '#') {
-        homePage.classList.remove('none');
-        this.document.querySelector('.blog-page').classList.add('none');
-    } else if (hash === '#blog') {
-        homePage.classList.add('none');
-        this.document.querySelector('.blog-page').classList.remove('none');
-    }
-});
-// Event for display notification
-window.addEventListener('hashchange', function() {
-    var hash = window.location.hash;
-    var homePage = document.querySelector('.blog-user-info');
-    var blog_page = this.document.querySelector('.notification');
-    if (hash === '#notification') {
-        homePage.classList.add('none');
+    } else if(!document.querySelector('.home-page').classList.contains('none')) {
+        document.querySelector('.home-page').classList.add('none');
         blog_page.classList.remove('none');
-    } else {
-        homePage.classList.remove('none');
-        blog_page.classList.add('none');
-
-    }
-    console.log(hash)
-    // Kiểm tra nếu không có hash hoặc hash là một hash rỗng thì hiển thị trang chính
-    if (!hash || hash === '#') {
-        homePage.classList.remove('none');
-        this.document.querySelector('.notification').classList.add('none');
     }
 });
 
-// Đảm bảo rằng trạng thái ban đầu của trang được xử lý đúng
-window.addEventListener('DOMContentLoaded', function() {
-    var hash = window.location.hash;
-    var homePage = document.querySelector('.blog-user-info');
 
-    if (!hash || hash === '#') {
-        homePage.classList.remove('none');
-        this.document.querySelector('.notification').classList.add('none');
-    } else if (hash === '#notification') {
-        homePage.classList.add('none');
-        this.document.querySelector('.notification').classList.remove('none');
+
+var noti_icon = document.querySelector('.nav_noti');
+noti_icon.addEventListener('click', function() {
+    if (document.querySelector('.blog-user-info').classList.contains('none')) {
+        document.querySelector('.blog-user-info').classList.remove('none');
+        document.querySelector('.notification').classList.add('none');
+    } else if(!document.querySelector('.blog-user-info').classList.contains('none')) {
+        document.querySelector('.blog-user-info').classList.add('none');
+        document.querySelector('.notification').classList.remove('none');
     }
 });
+
+document.querySelector('.nav_home').addEventListener('click',function(){
+    document.querySelector('.home-page').classList.remove('none');
+    blog_page.classList.add('none');
+})
