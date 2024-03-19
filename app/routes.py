bd3b8be01ab2ce3133c25b5b826495941fe0c769
xuanhathu <dangchithinhcungson@gmail.com>
2024-03-19 14:13:24 +0700

    var

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,37 @@
-from flask import render_template, url_for, flash, redirect, request, send_file, send_from_directory
+from flask import render_template, url_for, flash, redirect, request, send_file, send_from_directory, session
 from app import app
 import os
 import sqlite3  
 import bcrypt
 import uuid
+from werkzeug.utils import secure_filename
+from PIL import Image
 
 
 
 
 
+# Settings the utils
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 print(curr_dir)
 
-
+# Checks file extension
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# ConnectDB
 def getDB():
     conn = sqlite3.connect(os.path.join(curr_dir, "openu.db"))
     cursor = conn.cursor()
     return cursor, conn
 
 
-
+# Register route
 @app.route("/")
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    message = ""
     try:
-        message = ""  # Initialize an empty message
-        
         if request.method == "POST":
             emailAddr = request.form['email'].strip()
             username = request.form['username'].strip()
@@ -37,23 +43,65 @@ def register():
             if rows:
                 message = "User already exists"
             else:
+                # Generate ID and hash password
                 id = str(uuid.uuid4())
+                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                print(hashed_password)
+
+                # Adding data to db
                 query = "INSERT INTO user (id, username, emailAddr, password) VALUES (?, ?, ?, ?)"
-                cursor.execute(query, (id, username, emailAddr, password))
+                cursor.execute(query, (id, username, emailAddr, hashed_password))
                 conn.commit()
-                message = "Registration successful"  # Update the message
 
+                # Create folder to save user's uploads
+                user_upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], id)
+                if not os.path.exists(user_upload_folder):
+                    os.makedirs(user_upload_folder)
+
+                message = "Registration successful"
                 return redirect('/home')
-
+    # Catch error
     except Exception as error:
         print(f"ERROR: {error}", flush=True)
         return "You broke the server :(", 400
 
     return render_template("register.html", message=message)
 
-@app.route("/", methods=["GET", "POST"])
+
+
+# Login route
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    try:
+        if (request.method == "POST"):
+
+            emailAddr = request.form['email'].strip()
+            password = request.form['password'].strip()
+
+            # Get user data
+            cursor, conn = getDB()
+            user_info = cursor.execute("SELECT id, password FROM user WHERE emailAddr = ?",(emailAddr,)).fetchone()
+
+            if user_info:
+                id, hashed_password = user_info
+
+                # Check if password is equal to hash
+                if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+                    session['loggedin'] = True
+                    session['id'] = id
+                    file_path = os.path.join(app.config['UPLOAD_FOLDER'],session.get('id'))
+                    #notes_list = os.listdir(file_path)
+
+                    return redirect('/home')   
+                else:
+                     return render_template('login.html', message="Wrong Email or Password")
+            
+            return render_template('login.html', message="Wrong Email or Password")
+        return render_template("login.html")
+    except Exception as error:
+        print(f"ERROR: {error}", flush=True)
+        return "You broke the server :(", 400
+
 
 @app.route("/home")
 def home():
@@ -65,6 +113,51 @@ def profile():
 
 
 
-@app.route('/settings')
+@app.route('/settings', methods=["GET", "POST"])
 def settings():
-    return render_template('settings.html')
+    id = session['id']
+    cursor, conn = getDB()
+    user_info = cursor.execute("SELECT name, username, password, emailAddr FROM user WHERE id = ?",(id,)).fetchone()
+
+    name, username, hashed_password, emailAddr = user_info
+
+    if request.method == "GET":
+        return render_template('settings.html', name=name, username=username, email=emailAddr)
+    
+    elif request.method == "POST":
+        
+
+
+
+
+
+
+
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # Save the uploaded file
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            # Resize the image
+            resized_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'resized_' + filename)
+            with Image.open(file_path) as img:
+                img_resized = img.resize((130, 150))
+                img_resized.save(resized_file_path)
+
+            # Add code to update the user's profile picture in the database (if needed)
+            flash('File uploaded successfully')
+            return render_template('settings.html', profile_pic='resized_' + filename)  # Redirect to the settings page
+        
+        else:
+            flash('Invalid file format.')
+            return redirect(request.url)
